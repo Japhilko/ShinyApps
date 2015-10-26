af6fccbafac32283490d00ba8830b8065a5fb6a9
Jan-Philipp Kolb <Jan-Philipp_Kolb@gmx.net>
2015-10-26 09:33:55 +0100

    update more years

--- EurostatMapApp/server.R
+++ EurostatMapApp/server.R
@@ -5,13 +5,19 @@ GDP <- readRDS("data/EurostatGDP.rds")
 
 data(Europe)
 ind <- match(Europe@data$name,GDP$geo)
-Europe@data$X2012Q3 <- as.numeric(GDP$X2012Q3[ind])
-Europe@data$X2012Q4 <- as.numeric(GDP$X2012Q4[ind])
+
+Namen <- colnames(GDP)[-1]
+for(i in 1:length(Namen)){
+  eval(parse(text=paste("Europe@data$",Namen[i],
+    "<- as.numeric(GDP$",Namen[i],"[ind])",sep="")))
+}
+
+
 
 shinyServer(function(input,output) {
   output$map <- renderPlot({
-  data <- switch(input$var,"X2012Q3" = "X2012Q3",
-                 "X2012Q4" = "X2012Q4")
+    eval(parse(text=paste("data <- switch(input$var,",paste("'",Namen,"'='",Namen,"'",collapse=",",sep=""),
+                          ")",sep="")))
   qtm(Europe,data)
   })
 
--- EurostatMapApp/ui.R
+++ EurostatMapApp/ui.R
@@ -14,7 +14,7 @@ shinyUI(fluidPage(
   sidebarPanel(
     selectInput("var", 
                 label = "Choose a variable to display",
-                choices = c("X2012Q3", "X2012Q4"),
+                choices = c('X2012Q3','X2012Q4','X2013Q1','X2013Q2','X2013Q3','X2013Q4','X2014Q1','X2014Q2','X2014Q3','X2014Q4','X2015Q1','X2015Q2'),
                 selected = "X2012Q3")
   ),
   # Show a plot of the generated distribution
@@ -23,3 +23,4 @@ shinyUI(fluidPage(
   )
 )
 )
+# paste("'",Namen,"'",collapse=",",sep="")
